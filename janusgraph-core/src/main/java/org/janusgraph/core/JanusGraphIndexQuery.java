65d7f55ce4c373b536c801c6ae51cfbbaca819c0
Marco Scoppetta <marco@grakn.ai>
2019-10-18 16:20:30 +0100

    deleted unused classes

--- janusgraph-core/src/main/java/org/janusgraph/core/JanusGraphIndexQuery.java
+++ janusgraph-core/src/main/java/org/janusgraph/core/JanusGraphIndexQuery.java
@@ -15,9 +15,10 @@
 package org.janusgraph.core;
 
 import org.apache.tinkerpop.gremlin.process.traversal.Order;
-import org.janusgraph.core.schema.Parameter;
-import java.util.stream.Stream;
 import org.apache.tinkerpop.gremlin.structure.Element;
+import org.janusgraph.core.schema.Parameter;
+
+import java.util.stream.Stream;
 
 /**
  * A GraphQuery that queries for graph elements directly against a particular indexing backend and hence allows this
@@ -25,56 +26,40 @@ import org.apache.tinkerpop.gremlin.structure.Element;
  * However, the results returned by this query will not be adjusted to the modifications in a transaction. If there
  * are no changes in a transaction, this won't matter. If there are, the results of this query may not be consistent
  * with the transactional state.
- *
  */
 public interface JanusGraphIndexQuery {
 
     /**
      * Specifies the maximum number of elements to return
-     *
-     * @param limit
-     * @return
      */
     JanusGraphIndexQuery limit(int limit);
 
     /**
      * Specifies the offset of the query. Query results will be retrieved starting at the given offset.
-     * @param offset
-     * @return
      */
     JanusGraphIndexQuery offset(int offset);
 
     /**
      * Orders the element results of this query according
      * to their property for the given key in the given order (increasing/decreasing).
-     *
-     * @param key   The key of the properties on which to order
-     * @param order the ordering direction
-     * @return
      */
     JanusGraphIndexQuery orderBy(String key, Order order);
 
     /**
      * Adds the given parameter to the list of parameters of this query.
      * Parameters are passed right through to the indexing backend to modify the query behavior.
-     * @param para
-     * @return
      */
     JanusGraphIndexQuery addParameter(Parameter para);
 
     /**
      * Adds the given parameters to the list of parameters of this query.
      * Parameters are passed right through to the indexing backend to modify the query behavior.
-     * @param paras
-     * @return
      */
     JanusGraphIndexQuery addParameters(Iterable<Parameter> paras);
 
     /**
      * Adds the given parameters to the list of parameters of this query.
      * Parameters are passed right through to the indexing backend to modify the query behavior.
-     * @param paras
-     * @return
      */
     JanusGraphIndexQuery addParameters(Parameter... paras);
 
@@ -83,7 +68,6 @@ public interface JanusGraphIndexQuery {
      * in the query string.
      * <p>
      * For example, in the query 'v.name: Tom' the element identifier is 'v.'
-     *
      *
      * @param identifier The element identifier which must not be blank
      * @return This query builder
@@ -94,16 +78,12 @@ public interface JanusGraphIndexQuery {
      * Returns all vertices that match the query in the indexing backend.
      *
      * @deprecated use {@link #vertexStream()} instead.
-     *
-     * @return
      */
     @Deprecated
     Iterable<Result<JanusGraphVertex>> vertices();
 
     /**
      * Returns all vertices that match the query in the indexing backend.
-     *
-     * @return
      */
     Stream<Result<JanusGraphVertex>> vertexStream();
 
@@ -111,16 +91,12 @@ public interface JanusGraphIndexQuery {
      * Returns all edges that match the query in the indexing backend.
      *
      * @deprecated use {@link #edgeStream()} instead.
-     *
-     * @return
      */
     @Deprecated
     Iterable<Result<JanusGraphEdge>> edges();
 
     /**
      * Returns all edges that match the query in the indexing backend.
-     *
-     * @return
      */
     Stream<Result<JanusGraphEdge>> edgeStream();
 
@@ -128,56 +104,44 @@ public interface JanusGraphIndexQuery {
      * Returns all properties that match the query in the indexing backend.
      *
      * @deprecated use {@link #propertyStream()} instead.
-     *
-     * @return
      */
     @Deprecated
     Iterable<Result<JanusGraphVertexProperty>> properties();
 
-	/**
+    /**
      * Returns all properties that match the query in the indexing backend.
-     *
-     * @return
      */
     Stream<Result<JanusGraphVertexProperty>> propertyStream();
 
     /**
      * Returns total vertices that match the query in the indexing backend ignoring limit and offset.
-     *
-     * @return
      */
     Long vertexTotals();
 
     /**
      * Returns total edges that match the query in the indexing backend ignoring limit and offset.
-     *
-     * @return
      */
     Long edgeTotals();
 
     /**
      * Returns total properties that match the query in the indexing backend ignoring limit and offset.
-     *
-     * @return
      */
     Long propertyTotals();
-    
+
     /**
      * Container of a query result with its score.
+     *
      * @param <V>
      */
     interface Result<V extends Element> {
 
         /**
          * Returns the element that matches the query
-         *
-         * @return
          */
         V getElement();
 
         /**
          * Returns the score of the result with respect to the query (if available)
-         * @return
          */
         double getScore();
 
--- janusgraph-core/src/main/java/org/janusgraph/diskstorage/indexing/IndexTransaction.java
+++ janusgraph-core/src/main/java/org/janusgraph/diskstorage/indexing/IndexTransaction.java
@@ -15,8 +15,10 @@
 package org.janusgraph.diskstorage.indexing;
 
 import com.google.common.base.Preconditions;
-
-import org.janusgraph.diskstorage.*;
+import org.janusgraph.diskstorage.BackendException;
+import org.janusgraph.diskstorage.BaseTransaction;
+import org.janusgraph.diskstorage.BaseTransactionConfig;
+import org.janusgraph.diskstorage.LoggableTransaction;
 import org.janusgraph.diskstorage.util.BackendOperation;
 import org.janusgraph.graphdb.database.idhandling.VariableLong;
 import org.janusgraph.graphdb.database.serialize.DataOutput;
@@ -33,7 +35,6 @@ import java.util.stream.Stream;
 /**
  * Wraps the transaction handle of an index and buffers all mutations against an index for efficiency.
  * Also acts as a proxy to the {@link IndexProvider} methods.
- *
  */
 
 public class IndexTransaction implements BaseTransaction, LoggableTransaction {
@@ -47,37 +48,37 @@ public class IndexTransaction implements BaseTransaction, LoggableTransaction {
 
     private final Duration maxWriteTime;
 
-    private Map<String,Map<String,IndexMutation>> mutations;
+    private Map<String, Map<String, IndexMutation>> mutations;
 
     public IndexTransaction(IndexProvider index, KeyInformation.IndexRetriever keyInformation,
                             BaseTransactionConfig config,
                             Duration maxWriteTime) throws BackendException {
         Preconditions.checkNotNull(index);
         Preconditions.checkNotNull(keyInformation);
-        this.index=index;
+        this.index = index;
         this.keyInformation = keyInformation;
-        this.indexTx=index.beginTransaction(config);
+        this.indexTx = index.beginTransaction(config);
         Preconditions.checkNotNull(indexTx);
         this.maxWriteTime = maxWriteTime;
         this.mutations = new HashMap<>(DEFAULT_OUTER_MAP_SIZE);
     }
 
     public void add(String store, String documentId, IndexEntry entry, boolean isNew) {
-        getIndexMutation(store,documentId, isNew, false).addition(new IndexEntry(entry.field, entry.value, entry.getMetaData()));
+        getIndexMutation(store, documentId, isNew, false).addition(new IndexEntry(entry.field, entry.value, entry.getMetaData()));
     }
 
     public void add(String store, String documentId, String key, Object value, boolean isNew) {
-        getIndexMutation(store,documentId,isNew,false).addition(new IndexEntry(key,value));
+        getIndexMutation(store, documentId, isNew, false).addition(new IndexEntry(key, value));
     }
 
     public void delete(String store, String documentId, String key, Object value, boolean deleteAll) {
-        getIndexMutation(store,documentId,false,deleteAll).deletion(new IndexEntry(key,value));
+        getIndexMutation(store, documentId, false, deleteAll).deletion(new IndexEntry(key, value));
     }
 
     private IndexMutation getIndexMutation(String store, String documentId, boolean isNew, boolean isDeleted) {
         final Map<String, IndexMutation> storeMutations = mutations.computeIfAbsent(store, k -> new HashMap<>(DEFAULT_INNER_MAP_SIZE));
         IndexMutation m = storeMutations.get(documentId);
-        if (m==null) {
+        if (m == null) {
             m = new IndexMutation(keyInformation.get(store), isNew, isDeleted);
             storeMutations.put(documentId, m);
         } else {
@@ -91,7 +92,7 @@ public class IndexTransaction implements BaseTransaction, LoggableTransaction {
 
 
     public void register(String store, String key, KeyInformation information) throws BackendException {
-        index.register(store,key,information,indexTx);
+        index.register(store, key, information, indexTx);
     }
 
     /**
@@ -111,19 +112,19 @@ public class IndexTransaction implements BaseTransaction, LoggableTransaction {
      */
     @Deprecated
     public Iterable<RawQuery.Result<String>> query(RawQuery query) throws BackendException {
-        return new StreamIterable<>(index.query(query, keyInformation,indexTx));
+        return new StreamIterable<>(index.query(query, keyInformation, indexTx));
     }
 
     public Stream<RawQuery.Result<String>> queryStream(RawQuery query) throws BackendException {
-        return index.query(query, keyInformation,indexTx);
+        return index.query(query, keyInformation, indexTx);
     }
 
     public Long totals(RawQuery query) throws BackendException {
-        return index.totals(query, keyInformation,indexTx);
+        return index.totals(query, keyInformation, indexTx);
     }
 
-    public void restore(Map<String, Map<String,List<IndexEntry>>> documents) throws BackendException {
-        index.restore(documents, keyInformation,indexTx);
+    public void restore(Map<String, Map<String, List<IndexEntry>>> documents) throws BackendException {
+        index.restore(documents, keyInformation, indexTx);
     }
 
     @Override
@@ -134,12 +135,12 @@ public class IndexTransaction implements BaseTransaction, LoggableTransaction {
 
     @Override
     public void rollback() throws BackendException {
-        mutations=null;
+        mutations = null;
         indexTx.rollback();
     }
 
     private void flushInternal() {
-        if (mutations!=null && !mutations.isEmpty()) {
+        if (mutations != null && !mutations.isEmpty()) {
             //Consolidate all mutations prior to persistence to ensure that no addition accidentally gets swallowed by a delete
             for (Map<String, IndexMutation> store : mutations.values()) {
                 for (IndexMutation mut : store.values()) mut.consolidate();
@@ -158,26 +159,26 @@ public class IndexTransaction implements BaseTransaction, LoggableTransaction {
                 }
             }, maxWriteTime);
 
-            mutations=null;
+            mutations = null;
         }
     }
 
     @Override
     public void logMutations(DataOutput out) {
-        VariableLong.writePositive(out,mutations.size());
-        for (Map.Entry<String,Map<String,IndexMutation>> store : mutations.entrySet()) {
+        VariableLong.writePositive(out, mutations.size());
+        for (Map.Entry<String, Map<String, IndexMutation>> store : mutations.entrySet()) {
             out.writeObjectNotNull(store.getKey());
-            VariableLong.writePositive(out,store.getValue().size());
-            for (Map.Entry<String,IndexMutation> doc : store.getValue().entrySet()) {
+            VariableLong.writePositive(out, store.getValue().size());
+            for (Map.Entry<String, IndexMutation> doc : store.getValue().entrySet()) {
                 out.writeObjectNotNull(doc.getKey());
                 IndexMutation mut = doc.getValue();
-                out.putByte((byte)(mut.isNew()?1:(mut.isDeleted()?2:0)));
+                out.putByte((byte) (mut.isNew() ? 1 : (mut.isDeleted() ? 2 : 0)));
                 List<IndexEntry> additions = mut.getAdditions();
-                VariableLong.writePositive(out,additions.size());
-                for (IndexEntry add : additions) writeIndexEntry(out,add);
+                VariableLong.writePositive(out, additions.size());
+                for (IndexEntry add : additions) writeIndexEntry(out, add);
                 List<IndexEntry> deletions = mut.getDeletions();
-                VariableLong.writePositive(out,deletions.size());
-                for (IndexEntry del: deletions) writeIndexEntry(out,del);
+                VariableLong.writePositive(out, deletions.size());
+                for (IndexEntry del : deletions) writeIndexEntry(out, del);
             }
         }
     }
